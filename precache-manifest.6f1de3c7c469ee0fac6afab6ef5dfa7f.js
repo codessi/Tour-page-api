6905ff76b6f1ea84af489f4d921235acf6d807bd
codessi <johansuh@gmail.com>
2022-02-24 09:52:58 -0800

    Updates

---
+++ precache-manifest.6f1de3c7c469ee0fac6afab6ef5dfa7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf6bc99d0cb630d21d4031cff71bdbe",
+    "url": "/tour-page-api/index.html"
+  },
+  {
+    "revision": "53a3de37ba4f71fa7c51",
+    "url": "/tour-page-api/static/css/main.8326bf0e.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/tour-page-api/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tour-page-api/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53a3de37ba4f71fa7c51",
+    "url": "/tour-page-api/static/js/main.78bd0859.chunk.js"
+  },
+  {
+    "revision": "17bb2d7f61848a561e9b",
+    "url": "/tour-page-api/static/js/runtime-main.fa8e8488.js"
+  }
+]);
